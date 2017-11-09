8c4cc38c5e287b1a94016e680d62a7a18fd25f38
Viacheslav Ostroukh <v.dev@ostroukh.me>
2017-11-09 18:01:43 +0100

    release v0.1.1
        Bugfix: now should normally work if path contains spaces.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import exists
 from setuptools import setup
 
 setup(name='slurmified',
-      version='0.1.1.dev',
+      version='0.1.1',
       description='Dask.Didtributed on SLURM-managed clusters',
       url='https://gitlab.kwant-project.org/slavoutich/slurmified/',
       maintainer='Viacheslav Ostroukh',
